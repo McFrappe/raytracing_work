185f1290c55e95d63db7ab5df34d95e3ca33b192
mcfrappe <fahadrami.jamil.9983@student.uu.se>
2022-10-31 21:55:56 +0100

    feat: can now hit a sphere with rays

--- main.cpp
+++ main.cpp
@@ -1,27 +1,57 @@
-#include <iostream>
 #include "vec3.h"
+#include "ray.h"
 #include "color.h"
 
-#define _DIM 256
+#include <iostream>
+
+bool hit_sphere(const point3 &center, float radius, const ray &r) {
+  vec3 oc = r.origin() - center;
+
+  auto a = dot(r.direction(), r.direction());
+  auto b = 2.0 * dot(oc, r.direction());
+  auto c = dot(oc, oc) - radius*radius;
+  auto discriminant = b*b - 4*a*c;
+  return (discriminant > 0);
+}
+
+color ray_color(const ray &r) {
+  if (hit_sphere(point3(0,0,-1), 0.5, r))
+        return color(1, 0, 0);
+  vec3 unit_direction = unit_vector(r.direction());
+  float t = 0.5 * (unit_direction.y() + 1.0);
+  return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+}
 
 int main(int argc, char **argv) {
-	const int image_width = _DIM;
-	const int image_height = _DIM;
+  // image
+  const float aspect_ratio 	= 16.0 / 9.0;
+  const int image_width 	= 400;
+  const int image_height 	= static_cast<int>(image_width / aspect_ratio);
 
-	printf("P3\n%d %d\n255\n", image_width, image_height);
+  // camera viewpoint
+  float viewpoint_height 	= 2.0;
+  float viewpoint_width 	= aspect_ratio * viewpoint_height;
+  float focal_length 		= 1.0;
 
-	for (int i = image_height - 1; i >= 0; i--) {
-		std::cerr << "\nScanlines remaining: " << i  << ' ' << std::flush;
-		for (int j = 0; j < image_width; j++) {
-			color pixel_color(
-					float(j) / (image_width - 1),
-					float(i) / (image_height - 1),
-					0.25);
+  point3 origin 		= point3(0, 0, 0);
+  vec3 horizontal 		= vec3(viewpoint_width, 0, 0);
+  vec3 vertical 		= vec3(0, viewpoint_height, 0);
+  vec3 lower_left_corner 	= origin - horizontal/2 - vertical/2 - vec3(0, 0, focal_length);
 
-			write_color(std::cout, pixel_color);
-		}
-	}
+  // render image
+  printf("P3\n%d %d\n255\n", image_width, image_height);
 
-	std::cerr << "\nDone.\n";
-	return 0;
+  for (int i = image_height - 1; i >= 0; i--) {
+    std::cerr << "\nScanlines remaining: " << i  << ' ' << std::flush;
+    for (int j = 0; j < image_width; j++) {
+      float u = float(j) / (image_width - 1);
+      float v = float(i) / (image_height - 1);
+      ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+      color pixel_color = ray_color(r);
+      write_color(std::cout, pixel_color);
+    }
+  }
+
+  std::cerr << "\nDone.\n";
+  return 0;
 }
--- vec3.h
+++ vec3.h
@@ -69,6 +69,11 @@ inline std::ostream &operator<<(std::ostream &out, const vec3 &v) {
 }
 
 // define overloading for addition of vectors
+inline vec3 operator-(const vec3 &u, const vec3 &v) {
+	return vec3(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]);
+}
+
+// define overloading for addition of vectors
 inline vec3 operator+(const vec3 &u, const vec3 &v) {
 	return vec3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]);
 }
