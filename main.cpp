378b5ff3d84593db7b1fbc72a30e00c5201b87b9
mcfrappe <fahadrami.jamil.9983@student.uu.se>
2022-11-01 19:17:54 +0100

    fix/feat: refactored main.cpp with new class and list of objects, also, i had the indexes mixed up, making things rotated. Looks good now

--- main.cpp
+++ main.cpp
@@ -1,37 +1,19 @@
-#include "vec3.h"
-#include "ray.h"
+#include "tools.h"
+
 #include "color.h"
+#include "hittable_list.h"
+#include "sphere.h"
 
 #include <iostream>
+#include <memory>
 
-float hit_sphere(const point3 &center, float radius, const ray &r) {
-  vec3 oc = r.origin() - center;
-
-  float a = r.direction().length_squared();
-  float half_b = dot(oc, r.direction());
-  float c = oc.length_squared() - radius * radius;
-  float discriminant = half_b * half_b - a * c;
-
-  if (discriminant < 0) {
-    return -1.0;
-  } else {
-    return (-half_b - sqrt(discriminant))/ a;
-  }
-}
-
-color ray_color(const ray &r) {
-  float t = hit_sphere(point3(0, 0, -1), 0.5, r);
-  if (t > 0.0) {
-    vec3 normal = unit_vector(r.at(t) - vec3(0, 0, -1));
-    return 0.5 * color(
-	normal.x() + 1,
-	normal.y() + 1,
-	normal.z() + 1
-	);
-  }
+color ray_color(const ray &r, const hittable &world) {
+  hit_record rec;
+  if (world.hit(r, 0, infinity, rec))
+    return 0.5 * (rec.normal + color(1, 1, 1));
 
   vec3 unit_direction = unit_vector(r.direction());
-  t = 0.5*(unit_direction.y() + 1.0);
+  float t = 0.5 * (unit_direction.y() + 1.0);
   return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
 
@@ -40,6 +22,11 @@ int main(int argc, char **argv) {
   const float aspect_ratio 	= 16.0 / 9.0;
   const int image_width 	= 1200;
   const int image_height 	= static_cast<int>(image_width / aspect_ratio);
+
+  // world attr
+  hittable_list world;
+  world.add(std::make_shared<sphere>(point3(0, 0, -1), 0.5));
+  world.add(std::make_shared<sphere>(point3(0, -100.5, -1), 100));
 
   // camera viewpoint
   float viewpoint_height 	= 2.0;
@@ -57,10 +44,10 @@ int main(int argc, char **argv) {
   for (int i = image_height - 1; i >= 0; i--) {
     std::cerr << "\nScanlines remaining: " << i  << ' ' << std::flush;
     for (int j = 0; j < image_width; j++) {
-      float u = float(i) / (image_width - 1);
-      float v = float(j) / (image_height - 1);
+      float u = float(j) / (image_width - 1);
+      float v = float(i) / (image_height - 1);
       ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
-      color pixel_color = ray_color(r);
+      color pixel_color = ray_color(r, world);
       write_color(std::cout, pixel_color);
     }
   }
